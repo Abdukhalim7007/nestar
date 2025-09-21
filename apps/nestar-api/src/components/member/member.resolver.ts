bd9b93937103b72f355d1835ae1d25b3cb3551e6
Abdukhalim7007 <abduhalim.tech@gmail.com>
2025-09-21 15:39:36 +0900

    feat: create getAllMembersByAdmin and updateAnyMemberByAdmin grphQL APIs

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -18,7 +18,7 @@ export class MemberResolver {
 		console.log('Mutation: login');
 		return this.memberService.login(input);
 	}
-
+	// Authenticated Agent, User, Admin
 	@Mutation(() => String)
 	public async updateMember(): Promise<string> {
 		console.log('Mutation: updateMember');
@@ -29,5 +29,20 @@ export class MemberResolver {
 	public async getMember(): Promise<string> {
 		console.log('Query: getMember');
 		return this.memberService.getMember();
+	}
+
+	/** ADMIN **/
+
+	// Authorization: ADMIN
+	@Mutation(() => String)
+	public async getAllMembersByAdmin(): Promise<string> {
+		return this.memberService.getAllMembersByAdmin();
+	}
+
+	// Authorization: ADMIN
+	@Mutation(() => String)
+	public async updateAnyMemberByAdmin(): Promise<string> {
+		console.log('Mutation: updateAnyMemberByAdmin');
+		return this.memberService.updateAnyMemberByAdmin();
 	}
 }
--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -41,7 +41,7 @@ export class MemberService {
 		const isMatch = await this.authService.comparePasswords(input.memberPassword, response.memberPassword);
 		if (!isMatch) throw new InternalServerErrorException(Message.WRONG_PASSWORD);
 		response.accessToken = await this.authService.createToken(response);
-        
+
 		console.log('response:', response);
 
 		return response;
@@ -53,5 +53,13 @@ export class MemberService {
 
 	public async getMember(): Promise<string> {
 		return 'getMember executed!';
+	}
+
+	public async getAllMembersByAdmin(): Promise<string> {
+		return 'getAllMembersByAdmin executed!';
+	}
+
+	public async updateAnyMemberByAdmin(): Promise<string> {
+		return 'updateAnyMemberByAdmin executed!';
 	}
 }
